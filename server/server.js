d5093f877a146605f0b98c8a0e749ceb56f7a5ca
PrabinPoudel55 <prabin.poudel567@gmail.com>
2026-09-17 16:04:52 +0545

    set up express backend

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Weather App API is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
