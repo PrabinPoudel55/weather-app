bfe7bb720e28b612e72a69d43f4e0785564b7829
PrabinPoudel55 <prabin.poudel567@gmail.com>
2026-09-17 19:31:36 +0545

    Connect Express backend to MongoDB Atlas

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
+const dns = require("dns");
+
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({
