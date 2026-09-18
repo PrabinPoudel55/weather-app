8b31a4f90741f20d4022adace1f699d30f81bc00
PrabinPoudel55 <prabin.poudel567@gmail.com>
2026-09-18 11:49:27 +0545

    Enable CORS for frontend API requests

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const dns = require("dns");
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
 const weatherRoutes = require("./routes/weatherRoutes");
 
 // Use public DNS servers because of the DNS issue
-// we encountered on the W-Link network.
+// encountered on the W-Link network.
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 dotenv.config();
@@ -15,6 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
