ce40b4966624957330956b5cc91c06d62e91b9b3
PrabinPoudel55 <prabin.poudel567@gmail.com>
2026-09-18 15:13:03 +0545

    Add search history MongoDB model

---
+++ server/models/SearchHistory.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const searchHistorySchema = new mongoose.Schema(
+  {
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    countryCode: {
+      type: String,
+      trim: true,
+    },
+
+    latitude: {
+      type: Number,
+      required: true,
+    },
+
+    longitude: {
+      type: Number,
+      required: true,
+    },
+
+    timezone: {
+      type: String,
+      trim: true,
+    },
+
+    searchedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(
+  "SearchHistory",
+  searchHistorySchema
+);
